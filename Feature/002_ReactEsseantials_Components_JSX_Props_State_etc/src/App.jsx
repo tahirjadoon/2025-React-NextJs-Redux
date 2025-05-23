725394352bd43374679666ea734f8bd5e822fb6f
tahirjadoon <tjadoon@gmail.com>
2025-05-23 00:17:37 -0400

    02 Outputting dynamic content

--- Feature/002_ReactEsseantials_Components_JSX_Props_State_etc/src/App.jsx
+++ Feature/002_ReactEsseantials_Components_JSX_Props_State_etc/src/App.jsx
@@ -1,13 +1,25 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max){
+  const random =  Math.floor(Math.random() * (max + 1));
+  return random;
+}
+
+
 //moving the header out of App into Header
 //the function name starts with an upper case
 //either wrap the return inside () or do SHIFT+ALT+F to automatically add ()
 function Header() {
+  const random = genRandomInt(reactDescriptions.length -1);
+  const descripton = reactDescriptions[random];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {/* displaying the key word dynamically */}
+        {descripton} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
